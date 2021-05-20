e6820b4a9412a2e02c9694a655a281f9a11be4dc
H-hidetaka <sansanhidetaka@gmail.com>
2021-05-20 13:46:35 +0900

    [Modify]unit.rb追加

---
+++ retro.game_try/game6/lib/core/goomba.rb
@@ -0,0 +1,18 @@
+require 'core/unit'
+
+class Goomba < unit
+  attr_accessor :dead, :time_of_death
+  attr_reader :frame
+
+  def initialize window, x, y, map
+    super(window, x, y, map, 35, 30, :left)
+    @frame = 0
+    @dead = false
+    @time_of_death = 0
+  end
+
+  def update
+    @frame += 1 if @window.frame % 5 == 0
+    super
+  end
+end
---
+++ retro.game_try/game6/lib/core/unit.rb
@@ -0,0 +1,55 @@
+require 'core/map'
+
+class Unit
+  attr_accessor :x, :y, :velocity, :map, :height
+  attr_reader :width, :direction
+
+  def initialize window, x, y, map, width = 35, height = 30, direction = :right
+    @window = window
+    @map = map
+    @x, @y = x, y
+    @width, @height = width, height
+    @direction = direction
+    @velocity = 0
+  end
+
+  def update
+    case @direction
+      when :left
+        if fits?(-1, 0)
+          @x -= 1
+        else
+          @direction = :right
+        end
+      when :right
+        if fits?(1,0)
+          @x += 1
+        else
+          @direction = :left
+        end
+    end
+    move_y
+  end
+
+  def move_y
+    @velocity -= 1
+    if @velocity < 0
+      (-@velocity).times do
+        if fits?(0, 1)
+          @y += 1
+        else
+          @velocity = 0
+        end
+      end
+    end
+  end
+
+  def fits? offset_x, offset_y
+    (not @map.obsticle?(@x + offset_x + 5, @y + offset_y + 2)) and
+    (not @map.obsticle?(@x + offset_x + 5, @y + offset_y + @height /2 -1)) and
+    (not @map.obsticle?(@x + offset_x + 5, @y + offset_y + @height / -1)) and
+    (not @map.obsticle?(@x + offset_x - 6 * @width, @y + offset_y + 2)) and
+    (not @map.obsticle?(@x + offset_x - 6 * @witdh, @y + offset_y + @height / 2 -1)) and
+    (not @map.obsticle?(@x + offset_x - 6 * @width, @y + offset_y + @heifht -1))
+  end
+end
